52e49d3710ad0bf65866b51c5da4c00b4e882d96
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-15 06:51:20 +0800

    props added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,9 +7,22 @@ import Map from "./map.jsx";
 import flats from "../../data/flats.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: flats,
+      selectedFlat: null
+    }
+  }
+
   render() {
     return (
       <div>
+        <List
+          flats={this.state.flats}
+          selectedFlat = {this.state.selectedFlat}
+        />
+        <Map />
       </div>
     )
   }
