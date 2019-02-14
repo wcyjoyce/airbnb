df92e4b00c054a3d959673e115bc7a91e1d4c2fb
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-15 07:38:31 +0800

    dynamic image URL added

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Flat extends Component {
   render() {
     return (
-      <div className="card" style={ {backgroundImage: `url("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg")`} } >
+      // <div className="card" style={ {backgroundImage: `url("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg")`} } >
+      <div className="card" style={ {backgroundImage: `url(${this.props.flat.imageUrl})`} } >
         <div className="card-category">{this.props.flat.priceCurrency} {this.props.flat.price}</div>
         <div className="card-description">{this.props.flat.name}</div>
         <a className="class-link" href="#"></a>
