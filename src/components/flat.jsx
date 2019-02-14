f7cff566b4ee73dcd6f6f30d6fef9b06b4c6275a
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-15 07:11:11 +0800

    flat details displayed through props

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 class Flat extends Component {
   render() {
     return (
-      <div>
+      <div className="card" style={ {backgroundImage: `url("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg")`} } >
+        <div className="card-category">{this.props.flat.priceCurrency} {this.props.flat.price}</div>
+        <div className="card-description">{this.props.flat.name}</div>
+        <a className="class-link" href="#"></a>
       </div>
     )
   }
